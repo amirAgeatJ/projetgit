9e86e9d55f0e57c2abc3c72527ba03ed42461727
Lucas Ratiaray <dev@lucasratiaray.fr>
2024-07-02 00:52:16 +0200

    Test commit with lint errors

---
+++ eslint.config.cjs
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    files: ['**/*.js'],
+    rules: {
+      'semi': ['error', 'always'],
+      'no-unused-vars': ['error', { 'vars': 'all', 'args': 'after-used', 'ignoreRestSiblings': false }],
+      'no-var': 'error',
+      'indent': ['error', 2],
+      'quotes': ['error', 'double']
+    }
+  }
+];
