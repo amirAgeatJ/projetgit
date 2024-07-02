1de12034b4306a246819c76bd10c8acd37829d7c
Lucas Ratiaray <dev@lucasratiaray.fr>
2024-07-02 02:17:21 +0200

    modif conf for github action

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -2,11 +2,11 @@ module.exports = [
   {
     files: ['**/*.js'],
     rules: {
-      'semi': ['error', 'always'],
-      'no-unused-vars': ['error', { 'vars': 'all', 'args': 'after-used', 'ignoreRestSiblings': false }],
+      semi: ['error', 'always'],
+      'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
       'no-var': 'error',
-      'indent': ['error', 2],
-      'quotes': ['error', 'double']
+      indent: ['error', 2],
+      quotes: ['error', 'double']
     }
   }
-];
+]
