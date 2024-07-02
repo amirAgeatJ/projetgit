72866c906af9b92d826f1f450ee4ae091972e6f3
amir.ageat <ageatjamly.amir@gmail.com>
2024-07-02 02:36:07 +0200

    merge problem

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,6 +1,6 @@
 module.exports = [
   {
-    files: ['**/*.js'],
+    files: ['*/.js'],
     rules: {
       'semi': ['error', 'always'],
       'no-unused-vars': ['error', { 'vars': 'all', 'args': 'after-used', 'ignoreRestSiblings': false }],
